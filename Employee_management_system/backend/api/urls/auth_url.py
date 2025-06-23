b4bb00f4a6e7ec219a09038cf1ad105363a26685
Afsal897 <afsalsalim897@gmail.com>
2025-06-23 15:40:43 +0530

    refresh token route

--- Employee_management_system/backend/api/urls/auth_url.py
+++ Employee_management_system/backend/api/urls/auth_url.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from api.views import auth_view 
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register_admin/', auth_view.register_admin, name='register_admin'),
     path('register_user/', auth_view.register_employee_or_manager, name='register_employee_or_manager'),
     path('login/', auth_view.login_user, name='login_user'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     ]
